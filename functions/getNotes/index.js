09c2fa3bba27962bb6326ef421e82fc9e7936ae7
alkeal <yhma22.alkeal@folkuniversitetet.nu>
2023-11-29 15:15:36 +0100

    token

--- functions/getNotes/index.js
+++ functions/getNotes/index.js
@@ -1,6 +1,8 @@
+const { default: middy } = require('@middy/core');
 const { sendResponse } = require ('../../responses/index');
 const AWS = require('aws-sdk');
 const { send } = require('process');
+const { validateToken } = require('../middleware/auth');
 const db = new AWS.DynamoDB.DocumentClient();
 
 
@@ -32,3 +34,11 @@ exports.handler = async (event, contect ) => {
 
 
 }
+
+
+const handler = middy(getNotes)
+  .use(validateToken)
+
+
+
+module.exports = { handler };
